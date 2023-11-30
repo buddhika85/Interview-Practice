9b525e28e7f53606e0d65fc20165dda92f0b5040
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-12-01 03:46:31 +0530

    q1

---
+++ Answers/Q1TwoSumNum.cs
@@ -0,0 +1,56 @@
+﻿
+//using System.Collections.Generic;
+
+namespace Answers
+{
+    public class Q1TwoSumNum
+    {
+        // input = array [3,5-4,8,11,1,-1,0] is distinct, 10
+        // output = [-1,11]
+        public int[] TwoNumberSum(int[] array, int targetSum)
+        {
+            Dictionary<int, int> itemsDict = new();     // key = item, value = targetSum - value
+            foreach (var item in array)
+            {
+                var valueNeeded = targetSum - item;
+                if (itemsDict.ContainsKey(valueNeeded))
+                    return new[] { valueNeeded, item };
+                itemsDict.Add(item, item);
+
+            }
+
+            return new int[] { };
+        }
+
+        public int[] TwoNumberSumV2(int[] array, int targetSum)
+        {
+            Sort(array);
+            var left = 0;
+            var right = array.Length - 1;
+            while (left < right)
+            {
+                if (array[left] + array[right] < targetSum)
+                {
+                    // need more
+                    left++;
+                }
+                else if (array[left] + array[right] > targetSum)
+                {
+                    // need less
+                    right--;
+                }
+                else   // (array[left] + array[right] == targetSum)
+                {
+                    return new[] { array[left], array[right] };
+                }
+            }
+            return new int[] { };
+        }
+
+
+        public void Sort(int[] array)
+        {
+            Array.Sort(array);
+        }
+    }
+}
---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,14 @@
+﻿// Q1
+
+//var inputArray = new[] { 3, 5, -4, 8, 11, 1, -1, 0 };
+//var result = new Q1TwoSumNum().TwoNumberSum(inputArray, 10);
+//Write($"{result[0]} , {result[1]}");
+
+
+using Answers;
+
+var inputArray = new[] { 3, 5, -4, 8, 11, 1, -1, 0 };
+//new Q1TwoSumNum().Sort(inputArray);
+//WriteLine(string.Join(" | ", inputArray));
+var result = new Q1TwoSumNum().TwoNumberSumV2(inputArray, 10);
+Console.WriteLine($"{result[0]} , {result[1]}");
