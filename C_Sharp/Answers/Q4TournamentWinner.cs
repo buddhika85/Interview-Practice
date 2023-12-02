a94a7ccd20d1c7269add00441feac986ac1c241e
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-12-02 05:46:02 +0530

    C# q4

--- C_Sharp/Answers/Q4TournamentWinner.cs
+++ C_Sharp/Answers/Q4TournamentWinner.cs
@@ -7,34 +7,39 @@ namespace C_Sharp.Answers
 {
     public class Q4TournamentWinner
     {
-        public string TournamentWinner(List<List<string>> competitions, List<int> results)
+        // O(n) time and space
+        public string TournamentWinnerFirst(List<List<string>> competitions, List<int> results)
         {
             var teamScores = new Dictionary<string, int>();
-            string tournmentWinner = string.Empty;          
+            string tournmentWinner = string.Empty;
             for (var i = 0; i < competitions.Count; i++)
             {
                 var competition = competitions[i];
                 var winner = results[i] == 1 ? competition[0] : competition[1];
-                //Console.WriteLine($"{competition[0]} vs {competition[1]} => {winner}");
-                if (teamScores.ContainsKey(winner))
-                {
-                    teamScores[winner] = ++teamScores[winner];
-                }
-                else
-                {
-                    teamScores[winner] = 1;
-                }
+                teamScores[winner] = teamScores.ContainsKey(winner) ? ++teamScores[winner] : 1;
                 if (i == 0)
-                {
                     tournmentWinner = winner;
-                }
                 else
-                {
+                    if (teamScores[tournmentWinner] < teamScores[winner])
+                    tournmentWinner = winner;
+            }
+            return tournmentWinner;
+        }
+
+        public string TournamentWinner(List<List<string>> competitions, List<int> results)
+        {
+            var teamScores = new Dictionary<string, int>();
+            string tournmentWinner = string.Empty;
+            for (var i = 0; i < competitions.Count; i++)
+            {
+                var winner = results[i] == 1 ? competitions[i][0] : competitions[i][1];
+                teamScores[winner] = teamScores.ContainsKey(winner) ? ++teamScores[winner] : 1;
+                if (i == 0)
+                    tournmentWinner = winner;
+                else
                     if (teamScores[tournmentWinner] < teamScores[winner])
                         tournmentWinner = winner;
-                }
             }
-
             return tournmentWinner;
         }
     }
