dad176a7b3f76d889e0c2a109dda4b261d019848
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-01-01 08:41:22 +0530

    monotonic array test code using an enum

--- C_Sharp/Answers/Arrays/Monotonic.cs
+++ C_Sharp/Answers/Arrays/Monotonic.cs
@@ -7,21 +7,45 @@ namespace C_Sharp.Answers.Arrays
 {
     public class Monotonic
     {
+        // public static bool IsMonotonic(int[] array) {
+        //     if (array.Length <= 1)
+        //         return true;
+        //     bool? isIncreasing = null;      // do not know
+        //     for (int i = 0; i < array.Length - 1; i++)
+        //     {
+        //         if (isIncreasing == null)
+        //             isIncreasing = array[i] == array[i + 1] ? null : 
+        //                 array[i] < array[i + 1];
+        //         else if (isIncreasing.Value && array[i] > array[i + 1])
+        //             return false;           // we expected to increase, but did not
+        //         else if (!isIncreasing.Value && array[i] < array[i + 1])
+        //             return false;           // we expected to decrease, but did not
+        //     }
+        //     return isIncreasing != null;
+        // }
+
         public static bool IsMonotonic(int[] array) {
             if (array.Length <= 1)
                 return true;
-            bool? isIncreasing = null;      // do not know
+            MonotonicDirection monotonicDirection = MonotonicDirection.DontKnow;      // do not know
             for (int i = 0; i < array.Length - 1; i++)
             {
-                if (isIncreasing == null)
-                    isIncreasing = array[i] == array[i + 1] ? null : 
-                        array[i] < array[i + 1];
-                else if (isIncreasing.Value && array[i] > array[i + 1])
+                if (monotonicDirection == MonotonicDirection.DontKnow)
+                    monotonicDirection = array[i] == array[i + 1] ? MonotonicDirection.DontKnow : 
+                        array[i] < array[i + 1] ? MonotonicDirection.Increaase : MonotonicDirection.Decrease;
+                else if (monotonicDirection == MonotonicDirection.Increaase && array[i] > array[i + 1])
                     return false;           // we expected to increase, but did not
-                else if (!isIncreasing.Value && array[i] < array[i + 1])
+                else if (monotonicDirection == MonotonicDirection.Decrease && array[i] < array[i + 1])
                     return false;           // we expected to decrease, but did not
             }
-            return isIncreasing != null;
+            return monotonicDirection != MonotonicDirection.DontKnow;
+        }
+
+        enum MonotonicDirection
+        {
+            DontKnow,
+            Increaase,
+            Decrease
         }
     }
 }
