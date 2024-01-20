5e86161529ddacffacf0fa866a2b1841f299cd6d
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-01-20 16:19:40 +0530

    Node class added for Doubly Linked List Contruction

---
+++ C_Sharp/Answers/LLs/DLLs/Node.cs
@@ -0,0 +1,13 @@
+namespace C_Sharp.Answers.LLs.DLLs
+{
+    // Do not edit the class below.
+  public class Node {
+    public int Value;
+    public Node Prev;
+    public Node Next;
+
+    public Node(int value) {
+      this.Value = value;
+    }
+  }
+}
