630ecc264ae6a296be445ada5e16e7b41b718e03
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-01-30 11:48:10 +0530

    strings - minimum characters for words optimized

--- C_Sharp/Answers/Strings/MinimumCharactersForWordsDemo.cs
+++ C_Sharp/Answers/Strings/MinimumCharactersForWordsDemo.cs
@@ -15,10 +15,76 @@ namespace C_Sharp.Answers.Strings
             var obj = new MinimumCharactersForWordsDemo();
             var words = new string[] { "your", "you", "or", "yo" };
             WriteLine($"[{obj.MinimumCharactersForWords(words).Display(",")}]");
+            WriteLine($"[{obj.MinimumCharactersForWordsOptimal(words).Display(",")}]");
 
             words = new string[] { "this", "that", "did", "dead", "deed", "them!", "a" };
             WriteLine($"[{obj.MinimumCharactersForWords(words).Display(",")}]");
+            WriteLine($"[{obj.MinimumCharactersForWordsOptimal(words).Display(",")}]");
         }
+
+        public string[] MinimumCharactersForWordsOptimal(string[] words)
+        {
+            var charMaxCounts = new Dictionary<char, int>();
+            foreach (var word in words)
+            {
+                // find character count for a word
+                Dictionary<char, int> charCountsForWord = FindUniqueCharCountForWord(word);
+
+                // update common character counts
+                UpdateCharCountsForWords(charMaxCounts, charCountsForWord);
+            }
+            return GenerateAnArray(charMaxCounts);
+        }
+
+        private string[] GenerateAnArray(Dictionary<char, int> charMaxCounts)
+        {
+            var strings = new List<string>();
+            foreach (var pair in charMaxCounts)
+            {
+                for (int i = 0; i < pair.Value; i++)
+                {
+                    strings.Add(pair.Key.ToString());
+                }
+            }
+            return strings.ToArray();
+        }
+
+        private void UpdateCharCountsForWords(Dictionary<char, int> charMaxCounts, Dictionary<char, int> charCountsForWord)
+        {
+            foreach (var item in charCountsForWord)
+            {
+                if (charMaxCounts.ContainsKey(item.Key))
+                {
+                    if (charMaxCounts[item.Key] < item.Value)
+                    {
+                        charMaxCounts[item.Key] = item.Value;
+                    }
+                }
+                else
+                {
+                    charMaxCounts.Add(item.Key, item.Value);
+                }
+            }
+        }
+
+        private Dictionary<char, int> FindUniqueCharCountForWord(string word)
+        {
+            var charCountsForWord = new Dictionary<char, int>();
+            foreach (var character in word)
+            {
+                if (charCountsForWord.ContainsKey(character))
+                {
+                    ++charCountsForWord[character];
+                }
+                else
+                {
+                    charCountsForWord.Add(character, 1);
+                }
+            }
+
+            return charCountsForWord;
+        }
+
         public string[] MinimumCharactersForWords(string[] words)
         {
             var dictionary = new Dictionary<char, Counts>();
