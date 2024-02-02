d4ebdde8ef264ddbc91d055b0e66b44dda0c6c9d
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-02-02 12:08:30 +0530

    BST testing

--- C_Sharp/Answers/Bst/Construction/BstConstructionDemo.cs
+++ C_Sharp/Answers/Bst/Construction/BstConstructionDemo.cs
@@ -1,14 +1,46 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+
+using static System.Console;
 
 namespace C_Sharp.Answers.Bst.Construction
 {
     public static class BstConstructionDemo
     {
         public static void Demo()
-        { }
+        { 
+          BST bst = new BST(20);
+          bst.Insert(6);
+          bst.Insert(35);
+          bst.Insert(3);
+          bst.Insert(8);
+          bst.Insert(27);
+          bst.Insert(55);
+          bst.Insert(1);
+          bst.Insert(3);
+          bst.Insert(25);
+          bst.Insert(29);
+          bst.Insert(60);
+
+          WriteLine($"{bst.Contains(6)}");
+          WriteLine($"{bst.Contains(35)}");
+          WriteLine($"{bst.Contains(3)}");
+          WriteLine($"{bst.Contains(8)}");
+          WriteLine($"{bst.Contains(27)}");
+          WriteLine($"{bst.Contains(55)}");
+          WriteLine($"{bst.Contains(1)}");
+          WriteLine($"{bst.Contains(3)}");
+          WriteLine($"{bst.Contains(25)}");
+          WriteLine($"{bst.Contains(29)}");
+          WriteLine($"{bst.Contains(60)}");
+
+          WriteLine($"{bst.Contains(100)}");
+          WriteLine($"{bst.Contains(30)}");
+          WriteLine($"{bst.Contains(-1)}");
+        }
     }
 
     public class BST
@@ -51,6 +83,40 @@ namespace C_Sharp.Answers.Bst.Construction
             return this;
         }
 
+        public BST InsertIterative(int toInsert)
+        {
+          var tree = this;
+          while(true)
+          {
+            if (toInsert < tree.value)
+            {
+              // go left
+              if (tree.left == null)
+              {
+                tree.left = new BST(toInsert);
+                return this;
+              }
+              else
+              {
+                tree = tree.left;
+              }
+            }
+            else
+            {
+              // go right
+              if (tree.right == null)
+              {
+                tree.right = new BST(toInsert);
+                return this;
+              }
+              else
+              {
+                tree = tree.right;
+              }
+            }
+          }
+        }
+
         public bool Contains(int toSearch)
         {
             if (toSearch == value)
@@ -81,9 +147,39 @@ namespace C_Sharp.Answers.Bst.Construction
             }
         }
 
+        public bool ContainsIterative(int toSearch)
+        {
+          if (value == toSearch)
+            return true;
+
+          var tree = this;
+          while(tree != null)
+          {
+            if (toSearch  == tree.value)
+            {
+              return true;
+            }
+            else if (toSearch < tree.value)
+            {
+              tree = tree.left;
+            }
+            else
+            {
+              tree = tree.right;
+            }
+          }
+          return false;
+        }
+        
         public BST Remove(int value)
         {
-            // Write your code here.
+            // Find the Node
+            // 1 If the node to remove is a leaf - just remove it
+            // 2 If the node to remove has one child - remove it and put that child in the place of removed
+            // 3 If the node to remove is has 2 children - Find in order predesesor/successor of the node to remove 
+            //  3.1 remove it and in order predesesor/successor in the place of removed
+
+
             // Do not edit the return statement of this method.
             return this;
         }
--- C_Sharp/Program.cs
+++ C_Sharp/Program.cs
@@ -81,7 +81,9 @@ internal class Program
         //FindLoopDemo.Demo();
         //SunsetViewsDemo.Demo();
 
-        ReverseLinkedListDemo.Test();
+        //ReverseLinkedListDemo.Test();
+
+        BstConstructionDemo.Demo();
     }
 
     private static void TestDLL()
@@ -674,19 +676,19 @@ internal class Program
         WriteLine($"{Q25PalindromeCheck.IsPalindrome("xyx")}");
     }
 
-    private static void Q7()
-    {
-        var root = new BST(10);
-        root.left = new BST(5);
-        root.left.left = new BST(2);
-        root.left.left.left = new BST(1);
-        root.left.right = new BST(5);
-        root.right = new BST(15);
-        root.right.left = new BST(13);
-        root.right.left.right = new BST(14);
-        root.right.right = new BST(22);
-        WriteLine($"{Q7ClosestValueInBst.FindClosestValueInBst(root, 12)}");
-    }
+    // private static void Q7()
+    // {
+    //     var root = new BST(10);
+    //     root.left = new BST(5);
+    //     root.left.left = new BST(2);
+    //     root.left.left.left = new BST(1);
+    //     root.left.right = new BST(5);
+    //     root.right = new BST(15);
+    //     root.right.left = new BST(13);
+    //     root.right.left.right = new BST(14);
+    //     root.right.right = new BST(22);
+    //     WriteLine($"{Q7ClosestValueInBst.FindClosestValueInBst(root, 12)}");
+    // }
 
     private static void Q6()
     {
