cf451b62219bf9d58ab1b5afd93aac84197a72d1
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-02-22 14:50:43 +0530

    partment hunting job

---
+++ C_Sharp/Answers/Arrays/ApartmentHuntingDemo.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C_Sharp.Answers.Arrays
+{
+    public class ApartmentHuntingDemo
+    {
+
+        public static int ApartmentHunting(List<Dictionary<string, bool>> blocks, string[] reqs)
+        {
+            int minDistanceIndex = -1;
+            for (int i = 0; i < blocks.Count; i++)
+            {
+                foreach (var item in reqs)
+                {
+                    //if ()
+                }
+                
+            }
+            return minDistanceIndex;
+        }
+    }
+}
