f3fd959c82584618b9fcc1e2d80dabf7b4482183
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-03-01 16:43:12 +0530

    Two number sum

---
+++ Round_2/ArrayQs/TwoNumberSumDemo.cs
@@ -0,0 +1,29 @@
+﻿namespace Round_2;
+
+public class TwoNumberSumDemo
+{
+    public static int[] TwoNumberSum(int[] array, int targetSum)
+    {
+       
+        Array.Sort(array);
+        var left = 0;
+        var right = array.Length - 1;
+        while(left < right)
+        {
+            var sum = array[left] + array[right];
+            if (sum == targetSum)
+            {
+                return new int[]{array[left], array[right]};
+            }
+            else if (sum > targetSum)
+            {
+                --right;
+            }
+            else
+            {
+                ++left;
+            }
+        }
+        return new int[0];
+    }
+}
