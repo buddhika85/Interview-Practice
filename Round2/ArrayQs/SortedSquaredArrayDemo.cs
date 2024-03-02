6d5cfdee688577706709d69f6343bd69eaa8152e
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-03-02 05:47:32 +0530

    SortedSquaredArray

---
+++ Round2/ArrayQs/SortedSquaredArrayDemo.cs
@@ -0,0 +1,17 @@
+﻿namespace Round_2;
+
+public class SortedSquaredArrayDemo
+{
+    // TimeL O(n) | Space: O(n)
+    public int[] SortedSquaredArray(int[] array) 
+    {
+        var result = new int[array.Length];
+        for (int i = 0; i < array.Length; i++)  // O(n)
+        {
+            result[i] = array[i] * array[i];
+        }  
+        Array.Sort(result);     // quick sort - O(log n)
+        return result;  
+    }
+
+}
