5834bbc701fc6e011b786f45ed86d1deebd7b144
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-07-25 06:28:43 +1000

    2 number sum attempt 2

---
+++ Round2/ExtensionMethods/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Round_2.ExtensionMethods
+{
+    public static class Extensions
+    {
+        public static string Join<T>(this IEnumerable<T> items, char separator)
+        {
+            return string.Join(separator, items);
+        }
+    }
+}
--- Round2/Program.cs
+++ Round2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Round_2.ArrayQs;
+using Round_2.ExtensionMethods;
+using static System.Console;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        TestTwoNumberSum();
+    }
+
+    private static void TestTwoNumberSum()
+    {
+        var result = TwoNumberSum2.TwoNumberSum(new int[] { 3, 5, -4, 8, 11, -1, 6 }, 10);
+        WriteLine(result.Join<int>(','));
+
+    }
+}
