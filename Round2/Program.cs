3471bed9f060335cd1816873069e32704a08b3c8
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-07-28 04:38:30 +1000

    changes

--- Round2/Program.cs
+++ Round2/Program.cs
@@ -7,7 +7,15 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        TestTwoNumberSum();
+        //TestTwoNumberSum();
+        TestValidateSubsequence2();
+    }
+
+    private static void TestValidateSubsequence2()
+    {
+        var array = new List<int> { 5, 1, 22, 25, 6, -1, 8, 10 };
+        var sequence = new List<int> { 1, 6, -1, 5 };
+        WriteLine(ValidateSubsequence2.IsValidSubsequence(array, sequence));
     }
 
     private static void TestTwoNumberSum()
