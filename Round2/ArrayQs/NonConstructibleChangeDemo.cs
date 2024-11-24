8b11f85916ddf959997ddb6a3858248f8df3353d
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-11-24 21:53:11 +1100

    changes NonConstructibleChangeDemo

--- Round2/ArrayQs/NonConstructibleChangeDemo.cs
+++ Round2/ArrayQs/NonConstructibleChangeDemo.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Round_2.ArrayQs
 {
-    internal class NonConstructibleChange
+    internal class NonConstructibleChangeDemo
     {
+        //[1,2,5] => 4
+        //[5,7,1,1,2,3,22] => 20
+        // no coin => 1
+
+        //[5,7,1,1,2,3,22] => 20
+        //[1,1,2,3,5,7,22]
+        public int NonConstructibleChange(int[] coins)
+        {
+            Array.Sort(coins);
+            var minChange = 0;
+            foreach (var coin in coins) 
+            {
+                if (coin > minChange + 1)
+                    return minChange + 1;
+                minChange = coin + 1;
+            }
+            return minChange + 1;
+        }
     }
 }
