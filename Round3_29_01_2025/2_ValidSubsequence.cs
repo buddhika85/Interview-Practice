69e5454df3729d32180149c0256a1a5d54aaa729
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-01-30 16:32:09 +1100

    2 Is valid sub sequence

---
+++ Round3_29_01_2025/2_ValidSubsequence.cs
@@ -0,0 +1,23 @@
+﻿namespace Round3_29_01_2025
+{
+    internal class _2_ValidSubsequence
+    {
+        public static bool IsValidSubsequence(List<int> array, List<int> sequence)
+        {
+            if (array.Count < sequence.Count) 
+                return false;
+
+            var lookingForIndex = 0;
+            var arrayIndex = 0; 
+            while (lookingForIndex < sequence.Count && arrayIndex < array.Count)
+            {
+                if (array[arrayIndex] == sequence[lookingForIndex])
+                {
+                    lookingForIndex++;
+                }
+                arrayIndex++;
+            }
+            return lookingForIndex == array.Count;
+        }
+    }
+}
--- Round3_29_01_2025/Program.cs
+++ Round3_29_01_2025/Program.cs
@@ -6,7 +6,15 @@ namespace Round3_29_01_2025
     {
         static void Main(string[] args)
         {
-            Test_1();
+            //Test_1();
+            Test_2();
+        }
+
+        private static void Test_2()
+        {
+            var array = new[] { 55,1,22,25,6,-1,8,10 };
+            var sequence = new[] { 1, 6, -1, 10 };
+            WriteLine($"Is valid subsequence => {_2_ValidSubsequence.IsValidSubsequence(array.ToList(), sequence.ToList())}");
         }
 
         private static void Test_1()
