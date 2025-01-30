b322583dc8f3d3afc28ec81e93c251ce30fa33c8
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-01-30 17:01:45 +1100

    3 sorted squared array

---
+++ Round3_29_01_2025/3_SortedSquaredArray.cs
@@ -0,0 +1,29 @@
+﻿namespace Round3_29_01_2025
+{
+    internal class _3_SortedSquaredArray
+    {
+        public int[] SortedSquaredArray(int[] array)
+        {
+            var results = new int[array.Length];
+            var left = 0;
+            var right = array.Length - 1;
+            var resultsIndex = array.Length - 1;
+            while(resultsIndex >= 0)
+            {
+                var leftSquared = results[left] * results[left];
+                var rightSquared = results[right] * results[right];
+                if (leftSquared > rightSquared)
+                {
+                    results[resultsIndex--] = leftSquared;
+                    ++left;
+                }
+                else
+                {
+                    results[resultsIndex--] = rightSquared;
+                    --right;
+                }
+            }
+            return results;
+        }
+    }
+}
