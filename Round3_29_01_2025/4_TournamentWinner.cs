ab0c623e4144d5e62f381b6ad805f05606637408
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-02-03 21:11:42 +1100

    Tournament winner

---
+++ Round3_29_01_2025/4_TournamentWinner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Round3_29_01_2025
+{
+    internal class _4_TournamentWinner
+    {
+        public string TournamentWinner(List<List<string>> competitions, List<int> results)
+        {
+            // Write your code here.
+            return "";
+        }
+    }
+}
