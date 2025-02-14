9a37bafdcc0781cc207790bb4548ba362a877f3b
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-02-14 16:03:41 +1100

    NonConstructibleChange

---
+++ Round3_29_01_2025/5_NonConstructibleChange.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Round3_29_01_2025
+{
+    internal class _5_NonConstructibleChange
+    {
+        public int NonConstructibleChange(int[] coins)
+        {
+            if (coins == null || coins.Length == 0 || coins[1] > 1) 
+                return 1;
+            var lastMadeChange = 1;
+            for (var i = 1; i < coins.Length; i++)
+            {
+                if (coins[i] > ++lastMadeChange)
+                    return lastMadeChange;
+            }
+            return ++lastMadeChange;
+        }
+    }
+}
