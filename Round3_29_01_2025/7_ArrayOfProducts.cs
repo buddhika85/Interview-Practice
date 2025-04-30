b84548e444fdcf99468203f4e4a5b40545fbf1e1
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-04-30 10:15:05 +1000

    _7_ArrayOfProducts

--- Round3_29_01_2025/7_ArrayOfProducts.cs
+++ Round3_29_01_2025/7_ArrayOfProducts.cs
@@ -2,6 +2,8 @@
 {
     internal class _7_ArrayOfProducts
     {
+        // Time = O(n) -- because even though we have 2 loops they are'nt nested
+        // Space = O(n)  -- additional result array i used
         internal static int[] ArrayOfProducts(int[] array)
         {
             var result = new int[array.Length];
