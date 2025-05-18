674350f60a7a8d9671fa8672fd2551f5a06c64f8
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-05-19 09:42:34 +1000

    add two sum

---
+++ Round3_29_01_2025/11_TwoSum.cs
@@ -0,0 +1,30 @@
+﻿namespace Round3_29_01_2025
+{
+    internal class TwoSum
+    {
+        // You are given an array of Integers and another integer targetValue.
+        // Write a function that will take these inputs and return the indices of the 2 integers in the array that add up targetValue.
+        // [] => []
+        // [1,1,1],3 => []              // cannot make target 
+        // [1,2,3], 5 => [1,2]
+        // [2,1,3], 5 => [0,2]
+        // [2,1,-3], -1 => [0,2]
+        // [-2,1,-3],-5 => [0,2]        
+        public int[] GetTwoSum(int[] array, int target)
+        {
+            if (array == null || array.Length == 0)
+                return [];
+
+            var dictionary = new Dictionary<int, int[]>();      // how many for target, indexes
+            dictionary[target - array[0]] = [0];                
+            for (int i = 1; i < array.Length; i++)
+            {
+                var howManyToTarget = target - array[i];
+                if (dictionary.ContainsKey(howManyToTarget))
+                    return [dictionary[howManyToTarget][0], i];
+            }
+
+            return [];      // cannot find
+        }
+    }
+}
