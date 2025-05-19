a8d6465b188a2670090f742038a9991d0c23b4d3
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-05-19 10:28:41 +1000

    two sum tests

---
+++ Nunit.Tests/TwoSumTests.cs
@@ -0,0 +1,37 @@
+﻿using Round3_29_01_2025;
+
+
+namespace Nunit.Tests
+{
+    [TestFixture]
+    public class TwoSumTests
+    {
+        private TwoSum cut;
+
+        [SetUp]
+        public void setup()
+        {
+            cut = new TwoSum();
+        }
+
+        [Test]
+        [TestCase(new int[] { }, 0, new int[] { })]
+        [TestCase(new int[] { 25 }, 25, new int[] { })]
+        [TestCase(new int[] { 1, 1, 1 }, 3, new int[] { })]
+        [TestCase(new int[] { 2, 7 }, 9, new int[] { 0, 1 })]
+        [TestCase(new int[] { 2, 7, 3, -1, 4 }, 2, new int[] { 2, 3 })]
+        [TestCase(new int[] { 1, 2, 3 }, 5, new int[] { 1, 2 })]
+        [TestCase(new int[] { 2, 1, 3 }, 5, new int[] { 0, 2 })]
+        [TestCase(new int[] { 2, 1, -3 }, -1, new int[] { 0, 2 })]
+        [TestCase(new int[] { -2, 1, -3 }, -5, new int[] { 0, 2 })]
+        public void GetTwoSum_ReturnsCorrectIndexes_WhenTargetValuePassed(int[] array, int target, int[] indexes)
+        {
+            // A
+
+            // A
+            var actual = cut.GetTwoSum(array, target);
+            // A
+            Assert.That(indexes.SequenceEqual(actual));
+        }
+    }
+}
--- Round3_29_01_2025/11_TwoSum.cs
+++ Round3_29_01_2025/11_TwoSum.cs
@@ -1,10 +1,13 @@
 ﻿namespace Round3_29_01_2025
 {
-    internal class TwoSum
+    public class TwoSum
     {
         // You are given an array of Integers and another integer targetValue.
         // Write a function that will take these inputs and return the indices of the 2 integers in the array that add up targetValue.
         // [] => []
+        // [25], 25 => []
+        // [2,7], 9=> [0,1]
+        // [2,7,3,-1,4], 2 => [2,3]
         // [1,1,1],3 => []              // cannot make target 
         // [1,2,3], 5 => [1,2]
         // [2,1,3], 5 => [0,2]
@@ -12,18 +15,25 @@
         // [-2,1,-3],-5 => [0,2]        
         public int[] GetTwoSum(int[] array, int target)
         {
-            if (array == null || array.Length == 0)
+            if (array == null || array.Length < 2)
                 return [];
 
-            var dictionary = new Dictionary<int, int[]>();      // how many for target, indexes
-            dictionary[target - array[0]] = [0];                
-            for (int i = 1; i < array.Length; i++)
+            var dictionary = new Dictionary<int, int>();      // value, first index found
+                 
+            for (int i = 0; i < array.Length; i++)
             {
                 var howManyToTarget = target - array[i];
-                if (dictionary.ContainsKey(howManyToTarget))
-                    return [dictionary[howManyToTarget][0], i];
-            }
+                if (dictionary.TryGetValue(howManyToTarget, out int index))
+                {
+                    return [index, i];
+                }
 
+                // add to dictionary if the values index is not stored
+                if (!dictionary.ContainsKey(array[i]))
+                {
+                    dictionary[array[i]] = i;                    
+                }                
+            }
             return [];      // cannot find
         }
     }
