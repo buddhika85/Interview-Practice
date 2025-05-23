5f9638f2a5c0a5ff2a874f3caad005815b9f72a7
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-05-24 07:26:11 +1000

    IsIsomorphic_ReturnsCorrectResult_WhenTested

---
+++ Nunit.Tests/11_IsomorphicStringsTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Nunit.Tests
+{
+    [TestFixture]
+    public class IsomorphicStringsTests
+    {
+        private IsomorphicStrings cut;
+
+        [SetUp]
+        public void setup()
+        {
+            cut = new IsomorphicStrings();
+        }
+
+        [Test]
+        [TestCase("", "", true)]
+        [TestCase("abacus", "rirfgs", true)]
+        [TestCase("green", "abccd", true)]
+        [TestCase("aba", "pqr", false)]
+        [TestCase("ababr", "eoeoo", false)]
+        [TestCase("ababr", "pqrqo", false)]
+        public void IsIsomorphic_ReturnsCorrectResult_WhenTested(string source, string target, bool isIsomorphic)
+        {
+            // a
+            // a & a
+            Assert.That(cut.IsIsomorphic(source, target), Is.EqualTo(isIsomorphic));
+        }
+    }
+}
--- Nunit.Tests/GlobalUsings.cs
+++ Nunit.Tests/GlobalUsings.cs
@@ -1,1 +1,2 @@
 global using NUnit.Framework;
+global using Round3_29_01_2025;
