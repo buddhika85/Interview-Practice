205ff5b6538e53b0cbc7c0e0c9df17bac9a95902
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-05-24 07:05:04 +1000

    IsIsomorphic

--- Round3_29_01_2025/11_IsomorphicStrings.cs
+++ Round3_29_01_2025/11_IsomorphicStrings.cs
@@ -5,8 +5,29 @@
         // "Given two strings s and t, determine if they are isomorphic. Two strings s and t are isomorphic if the characters in s can be replaced to get t.
         // All occurrences of a character must be replaced with another character while preserving the order of characters.
         // //No two characters may map to the same character, but a character may map to itself.s and t consist of any valid ascii character. "
+        // abacus, rirfgs => true
+        // aba, pqr => false
+        // ababr, eoeoo => false
+        // ababr, pqrqo => false
+        // green, abccd => true
         public bool IsIsomorphic(string s, string t)
         {
+            if (s.Length != t.Length)
+                return false;
+
+            Dictionary<char, char> dict = new ();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dict.ContainsKey(s[i]))
+                {
+                    if (dict[s[i]] != t[i])
+                        return false;
+                }
+                else
+                {
+                    dict[s[i]] = t[i];
+                }
+            }
             return true;
         }
     }
