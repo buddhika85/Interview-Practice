49f6c02c75d459cb03ded232a70068fbf45b7c3d
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-05-24 07:25:47 +1000

    possible solution with test cases

--- Round3_29_01_2025/11_IsomorphicStrings.cs
+++ Round3_29_01_2025/11_IsomorphicStrings.cs
@@ -6,26 +6,38 @@
         // All occurrences of a character must be replaced with another character while preserving the order of characters.
         // //No two characters may map to the same character, but a character may map to itself.s and t consist of any valid ascii character. "
         // abacus, rirfgs => true
+        // green, abccd => true
         // aba, pqr => false
         // ababr, eoeoo => false
         // ababr, pqrqo => false
-        // green, abccd => true
+
         public bool IsIsomorphic(string s, string t)
         {
             if (s.Length != t.Length)
                 return false;
 
-            Dictionary<char, char> dict = new ();
+            Dictionary<char, char> dictST = new ();
+            Dictionary<char, char> dictTS = new();
             for (int i = 0; i < s.Length; i++)
             {
-                if (dict.ContainsKey(s[i]))
+                if (dictST.ContainsKey(s[i]))
                 {
-                    if (dict[s[i]] != t[i])
+                    if (dictST[s[i]] != t[i])
                         return false;
                 }
                 else
                 {
-                    dict[s[i]] = t[i];
+                    dictST[s[i]] = t[i];
+                }
+
+                if (dictTS.ContainsKey(t[i]))
+                {
+                    if (dictTS[t[i]] != s[i])
+                        return false;
+                }
+                else
+                {
+                    dictTS[t[i]] = s[i];
                 }
             }
             return true;
